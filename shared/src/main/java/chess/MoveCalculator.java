8452ebdc470ef74e232e47bf47ec55970b2970ae
Carter Nelson <cartern215@gmail.com>
2025-05-04 00:48:15 -0600

    Worked more on Bishop movement algorithm

--- shared/src/main/java/chess/MoveCalculator.java
+++ shared/src/main/java/chess/MoveCalculator.java
@@ -1,7 +1,6 @@
 package chess;
 
 import java.util.Collection;
-import java.util.List;
 
 public interface MoveCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition);
@@ -34,12 +33,57 @@ class RookMoves implements MoveCalculator {
 
 class BishopMoves implements MoveCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        ChessMove[] possibleMoves = {};
+        Collection<ChessMove> possibleMoves = new Collection<ChessMove>;
         int currentRow = myPosition.getRow();
         int currentCol = myPosition.getColumn();
+        ChessPosition startingPosition = new ChessPosition(currentRow, currentCol);
+        int counter = 0;
+        while(currentRow != 7 & currentCol != 0) {
+            currentRow += 1;
+            currentCol -=1;
+            ChessPosition endingPosition = new ChessPosition(currentRow, currentCol);
+            ChessMove move = new ChessMove(startingPosition, endingPosition, null);
+            possibleMoves[0][counter] = move;
+            counter++;
+        }
+        currentRow = myPosition.getRow();
+        currentCol = myPosition.getColumn();
 
+        counter = 0;
+        while(currentRow != 0 & currentCol != 0) {
+            currentRow -= 1;
+            currentCol -=1;
+            ChessPosition endingPosition = new ChessPosition(currentRow, currentCol);
+            ChessMove move = new ChessMove(startingPosition, endingPosition, null);
+            possibleMoves[1][counter] = move;
+            counter++;
+        }
+        currentRow = myPosition.getRow();
+        currentCol = myPosition.getColumn();
 
-        return List.of(possibleMoves);
+        counter = 0;
+        while(currentRow != 7 & currentCol != 0) {
+            currentRow -= 1;
+            currentCol +=1;
+            ChessPosition endingPosition = new ChessPosition(currentRow, currentCol);
+            ChessMove move = new ChessMove(startingPosition, endingPosition, null);
+            possibleMoves[2][counter] = move;
+            counter++;
+        }
+        currentRow = myPosition.getRow();
+        currentCol = myPosition.getColumn();
+
+        counter = 0;
+        while(currentRow != 7 & currentCol != 7) {
+            currentRow += 1;
+            currentCol +=1;
+            ChessPosition endingPosition = new ChessPosition(currentRow, currentCol);
+            ChessMove move = new ChessMove(startingPosition, endingPosition, null);
+            possibleMoves[3][counter] = move;
+            counter++;
+        }
+        
+        return possibleMoves;
     }
 }
 
