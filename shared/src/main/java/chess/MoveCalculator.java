9be7381065e376691bacf79cf4d52983dd7c1148
Carter Nelson <cartern215@gmail.com>
2025-05-05 14:23:11 -0600

    Implemented logic for all other pieces besides pawn

--- shared/src/main/java/chess/MoveCalculator.java
+++ shared/src/main/java/chess/MoveCalculator.java
@@ -10,25 +10,107 @@ public interface MoveCalculator {
 
 class KingMoves implements MoveCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-
+        Collection<ChessMove> possibleMoves = new ArrayList<ChessMove>(7);
+        OffSet offSet = new OffSet();
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 7; j++) {
+                ChessPosition endPosition = offSet.applyOffSet(myPosition, offSet.kingOffSets[i]);
+                if(endPosition.getRow() != -1 && endPosition.getColumn() != -1) {
+                    ChessMove validMove = new ChessMove(endPosition, myPosition, null);
+                    int validationCode = validMove.checkPosition(board.getBoard());
+                    if(validationCode == 1) {
+                        possibleMoves.add(validMove);
+                    } else {
+                        if(validationCode == 2) {
+                            possibleMoves.add(validMove);
+                        }
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        return possibleMoves;
     }
 }
 
 class QueenMoves implements MoveCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-
+        Collection<ChessMove> possibleMoves = new ArrayList<ChessMove>(7);
+        OffSet offSet = new OffSet();
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 7; j++) {
+                ChessPosition endPosition = offSet.applyOffSet(myPosition, offSet.queenOffSets[i]);
+                if(endPosition.getRow() != -1 && endPosition.getColumn() != -1) {
+                    ChessMove validMove = new ChessMove(endPosition, myPosition, null);
+                    int validationCode = validMove.checkPosition(board.getBoard());
+                    if(validationCode == 1) {
+                        possibleMoves.add(validMove);
+                    } else {
+                        if(validationCode == 2) {
+                            possibleMoves.add(validMove);
+                        }
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        return possibleMoves;
     }
 }
 
 class KnightMoves implements MoveCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-
+        Collection<ChessMove> possibleMoves = new ArrayList<ChessMove>(8);
+        OffSet offSet = new OffSet();
+        for (int i = 0; i < 8; i++) {
+            ChessPosition endPosition = offSet.applyOffSet(myPosition, offSet.knightOffSets[i]);
+            if (endPosition.getRow() != -1 && endPosition.getColumn() != -1) {
+                ChessMove validMove = new ChessMove(endPosition, myPosition, null);
+                int validationCode = validMove.checkPosition(board.getBoard());
+                if(validationCode == 1) {
+                    possibleMoves.add(validMove);
+                } else {
+                    if (validationCode == 2) {
+                        possibleMoves.add(validMove);
+                    }
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        return possibleMoves;
     }
 }
 
 class RookMoves implements MoveCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-
+        Collection<ChessMove> possibleMoves = new ArrayList<ChessMove>(7);
+        OffSet offSet = new OffSet();
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 7; j++) {
+                ChessPosition endPosition = offSet.applyOffSet(myPosition, offSet.rookOffSets[i]);
+                if(endPosition.getRow() != -1 && endPosition.getColumn() != -1) {
+                    ChessMove validMove = new ChessMove(endPosition, myPosition, null);
+                    int validationCode = validMove.checkPosition(board.getBoard());
+                    if(validationCode == 1) {
+                        possibleMoves.add(validMove);
+                    } else {
+                        if(validationCode == 2) {
+                            possibleMoves.add(validMove);
+                        }
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        return possibleMoves;
     }
 }
 
@@ -73,6 +155,7 @@ class OffSet {
     int[][] knightOffSets = {{2, -1}, { 1, -2}, { -1, -2}, {-2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}};
     int[][] rookOffSets = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
     int[][] queenOffSets = {{1, -1}, {-1, -1}, {-1, 1}, {1, 1}, {0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+    int[][] kingOffSets = {{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
 
     public ChessPosition applyOffSet(ChessPosition start, int[] changes) {
         int endRow = -1;
