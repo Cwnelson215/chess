29531a3662888141d77c323ca826003069d60588
cwnel215 <cartern215@gmail.com>
2025-06-03 12:56:40 -0600

    Added register login and help methods to ChessClient

--- client/src/main/java/ChessClient.java
+++ client/src/main/java/ChessClient.java
@@ -1,3 +1,5 @@
+import model.UserData;
+
 import java.util.Arrays;
 import java.util.Locale;
 
@@ -17,10 +19,50 @@ public class ChessClient {
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (cmd) {
-                case
-            }
-        } catch (RuntimeException e) {
+                case "register" -> register(params);
+                case "login" -> login(params);
+
+                default -> help();
+            };
+        } catch (ResponseException e) {
             return e.getMessage();
         }
+    }
+
+    public String register(String... params) throws ResponseException {
+        if(params.length >= 1) {
+            state = State.LOGGEDIN;
+            var result = server.register(new UserData(params[0], params[1], params[2]));
+            return String.format("Logged in as %s", result.toString());
+        }
+        throw new ResponseException(400, "Expected: <username> <password> <email>");
+    }
+
+    public String login(String...params) throws ResponseException {
+        if(params.length >= 1) {
+            state = State.LOGGEDIN;
+            var result = server.login(new UserData(params[0], params[1], params[2]));
+            return String.format("You logged in as %s", result.getUsername());
+        }
+        throw new ResponseException(400, "Expected: <username> <password>");
+    }
+
+    public String help() {
+        if(state == State.LOGGEDOUT) {
+            return """
+                    - help
+                    - quit
+                    - login <USERNAME> <PASSWORD>
+                    - register <USERNAME> <PASSWORD> <EMAIL>
+                    """;
+        }
+        return """
+                - help
+                - create <NAME>
+                - logout
+                - list
+                - join <ID> [WHITE|BLACK]
+                - observe <ID>
+                """;
     }
 }
